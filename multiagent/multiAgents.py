b7a7883fe6b44662e9731341fe91749cead434f5
Edvard Bjørnevik <edvardbj@stud.ntnu.no>
2022-10-13 20:52:22 +0200

    Added MiniMax and MiniMax with alpha-beta pruning

--- multiagent/multiAgents.py
+++ multiagent/multiAgents.py
@@ -15,6 +15,7 @@
 from util import manhattanDistance
 from game import Directions
 import random, util
+import math
 
 from game import Agent
 
@@ -135,9 +136,51 @@ class MinimaxAgent(MultiAgentSearchAgent):
         Returns whether or not the game state is a losing state
         """
         "*** YOUR CODE HERE ***"
+
+        #Returns the best move (search restricted by depth)
+        def Minimax_Search(depth, gamestate):
+            value, move = Max_Value(depth, gamestate)
+            return move
+
+        def Max_Value(depth, gameState): #Returns a (utility, move) pair
+            #Checks if game is in terminal state, or if it has reached the depth of the tree. 
+            if gameState.isWin() or (gameState.isLose()) or (depth == 0):
+                return scoreEvaluationFunction(gameState), gameState
+
+            v = float(-math.inf) #Initial v value
+
+            #Checks the scoreEvaluation, of the moves, from min-agent for max-agent´s legal actions, i.e., possible moves.
+            # Then chooses the max (best move) of min-agent´s max-outcome.
+            for validMove in gameState.getLegalActions(0):
+                v2, a2 = Min_Value(self, gameState.generateSuccessor(self.index, validMove), gameState.getNumAgents()-1, 1,validMove, depth-1)
+                if v2 > v:
+                    v, move = v2, validMove
+            return v, move
+        
+        def Min_Value(self, gameState, numOfGhosts, ghostCounter, move, depth): #Returns a (utility, move) pair
+            #Checks if game is in terminal state
+            if gameState.isWin() or (gameState.isLose()):
+                return scoreEvaluationFunction(gameState), move
+
+            v = float(math.inf) #Initial v value
+
+            #Does the same as Max_value, but also calls on itself for the number of ghosts present. e.g., 2 ghosts will make it call on itself one time
+            # Then chooses the max (best move, which is the min) of max-agent´s max-outcome.
+            for validMove in gameState.getLegalActions(ghostCounter):
+                #Makes sure that Max is only called when every agent has made an action.
+                if (numOfGhosts == ghostCounter):
+                    v2, a2 = Max_Value(depth, gameState.generateSuccessor(ghostCounter, validMove))
+                else:
+                    v2, a2 = Min_Value(self, gameState.generateSuccessor(ghostCounter, validMove), numOfGhosts, ghostCounter+1, validMove, depth)
+                if v2 < v:
+                    v, move = v2, validMove
+            return v, move
+
+        #Returns the best move
+        return Minimax_Search(self.depth, gameState)
         util.raiseNotDefined()
 
-class AlphaBetaAgent(MultiAgentSearchAgent):
+class AlphaBetaAgent(MultiAgentSearchAgent): #Code here
     """
     Your minimax agent with alpha-beta pruning (question 3)
     """
@@ -147,6 +190,59 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
         Returns the minimax action using self.depth and self.evaluationFunction
         """
         "*** YOUR CODE HERE ***"
+
+        #Returns the best move (search restricted by depth)
+        def Minimax_Search(depth, gamestate): #Returns an action
+            value, move = Max_Value(depth, gamestate, float(-math.inf), float(math.inf))
+            return move
+
+        def Max_Value(depth, gameState, alfa, beta): #Returns a (utility, move) pair
+            #Checks if game is in terminal state, or if it has reached the depth of the tree. 
+            if gameState.isWin() or (gameState.isLose()) or (depth == 0):
+                return scoreEvaluationFunction(gameState), gameState
+
+            v = float(-math.inf) #Initial v value
+
+            #Checks the scoreEvaluation, of the moves, from min-agent for max-agent´s legal actions, i.e., possible moves.
+            # Then chooses the max (best move) of min-agent´s max-outcome.
+            for validMove in gameState.getLegalActions(0):
+                v2, a2 = Min_Value(self, gameState.generateSuccessor(self.index, validMove), gameState.getNumAgents()-1, 1,validMove, depth-1, alfa, beta)
+                
+                #Alpha-beta pruning
+                #Returns only the (utility, move) pair that is better than beta. I.e., the new utility score must be better that the last, but also greater than what Min_value (beta) has as its best.
+                if v2 > v:
+                    v, move = v2, validMove
+                    alfa = max(alfa, v)
+                if v > beta:
+                    return v, move
+            return v, move
+        
+        def Min_Value(self, gameState, numOfGhosts, ghostCounter, move, depth, alfa, beta): #Returns a (utility, move) pair
+             #Checks if game is in terminal state
+            if gameState.isWin() or (gameState.isLose()):
+                return scoreEvaluationFunction(gameState), move
+
+            v = float(math.inf) #Initial v value
+
+            #Does the same as Max_value, but also calls on itself for the number of ghosts present. e.g., 2 ghosts will make it call on itself one time
+            # Then chooses the max (best move, which is the min) of max-agent´s max-outcome.
+            for validMove in gameState.getLegalActions(ghostCounter):
+                #Makes sure that Max is only called when every agent has made an action.
+                if (numOfGhosts == ghostCounter):
+                    v2, a2 = Max_Value(depth, gameState.generateSuccessor(ghostCounter, validMove), alfa, beta)
+                else:
+                    v2, a2 = Min_Value(self, gameState.generateSuccessor(ghostCounter, validMove), numOfGhosts, ghostCounter+1, validMove, depth, alfa, beta)
+                
+                #Alpha-beta pruning
+                #Returns only the (utility, move) pair that is better than alfa. I.e., the new utility score must be better than the previous, but also less than what Max_value (alfa) has as its best.
+                if v2 < v:
+                    v, move = v2, validMove
+                    beta = min(beta, v)
+                if v < alfa:
+                    return v, move
+            return v, move
+
+        return Minimax_Search(self.depth, gameState)
         util.raiseNotDefined()
 
 class ExpectimaxAgent(MultiAgentSearchAgent):
